6a2abc51cad1ef027fc3c1f7bbcd343fa189cf38
Filip Sarosiek <Filip.Sarosiek@hrs.com>
2019-01-18 23:09:50 +0100

    added SavingAccount child class

--- src/com/filip/Main.java
+++ src/com/filip/Main.java
@@ -6,22 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Account test = new Account("fra","Test account", 100);
+//        Account test = new Account();
+//
+//        System.out.println(test);
+//        System.out.println(test.getBalance());
+//
+//        SavingAccount lokata = new SavingAccount("Filip","PLN",100, 2.5, 5);
+//        System.out.println(lokata);
+//        lokata.withdrawal(101);
+//        lokata.withdrawal(50.5);
+//        System.out.println(lokata);
 
-        System.out.println(test);
-        System.out.println(test.getBalance());
 
-        test.deposit(50.6);
-        System.out.println(test);
-        System.out.println(test.getBalance());
-
-        test.withdrawal(200);
-        System.out.println(test);
-        System.out.println(test.getBalance());
-
-        test.withdrawal(70.594999999999);
-        System.out.println(test);
-        System.out.println(test.getBalance());
 
 
 
---
+++ src/com/filip/SavingAccount.java
@@ -0,0 +1,43 @@
+package com.filip;
+
+import java.math.*;
+
+public class SavingAccount extends Account {
+
+    private BigDecimal interestRate;
+    private int availWithdrawals;
+
+    public SavingAccount(String owner, String currency, double balance, double interestRate,
+                         int availWithdrawals) throws IllegalArgumentException {
+        super("Saving Account", owner, currency, balance );
+
+        if (availWithdrawals>0 || interestRate>0) {
+            this.availWithdrawals = availWithdrawals;
+            this.interestRate = round(interestRate);
+        }
+        else {
+            throw new IllegalArgumentException("InterestRate and availWithdrawals must be greater than 0");
+        }
+    }
+
+    @Override
+    public void withdrawal(double amount) {
+        if (this.availWithdrawals==0) {
+            System.out.println("You've exceeded withdrawals limit.");
+        }
+        else if (amount>this.getBalance().doubleValue()/100) {
+            System.out.println("Not enough funds.");
+        }
+        else {
+            super.withdrawal(amount);
+            this.availWithdrawals--;
+        }
+    }
+
+    public String toString() {
+
+        return "\n" + super.toString() + "\nInterest Rate: " + this.interestRate +
+                "\nAvailable withdrawals: " + availWithdrawals;
+    }
+
+}
